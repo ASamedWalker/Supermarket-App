72a9cb4500d78494c82eda30e1af4c93a7ebbf2f
Samed Walker <samedwalker50@gmail.com>
2022-03-04 22:05:31 -0500

    Added new files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
+import Button from "./components/UI/Button";
 
-function App() {
+const App = () => {
+  const handleButtonClick = () => {
+    console.log("button Clicked");
+  };
+
   return (
-    <div>
-      <h1>Welcome to my page</h1>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "20px",
+        flexWrap: "wrap",
+      }}
+    >
+      <Button>Normal</Button>
+      <Button outline>Outline</Button>
+      <Button className="extra-class" onClick={handleButtonClick}>
+        Customizable
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,1 +1,18 @@
-import Reat from "react";
+import React from "react";
+import clsx from "clsx";
+import classes from "./Button.module.css";
+
+const Button = (props) => {
+  const {children, outline, className, ...rest} = props;
+  const classes = clsx({
+    "btn": true,
+    "btn-outline": outline,
+    "btn-default": !outline,
+  }, className)
+  return (
+    <button className={classes}{...rest}>{children}</button>
+  );
+
+}
+
+export default Button;
